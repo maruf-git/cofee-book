a26b2c5e7b6da1a0ff967a98b967cfd73fd2a7bc
orion <munna.csehstu@gmail.com>
2024-11-03 12:02:47 +0600

    completed navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import router from './components/Router'
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <RouterProvider router={router} />
   </StrictMode>,
 )
