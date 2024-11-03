146a72e3867fc3f90c57199f09b3d3d4e2169056
orion <munna.csehstu@gmail.com>
2024-11-03 12:09:45 +0600

    added children components for main layout

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,11 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import MainLayout from "../layouts/MainLayout";
+import Coffees from "./Coffees";
+import Dashboard from "./Dashboard";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <MainLayout></MainLayout>
+        element: <MainLayout></MainLayout>,
+        children:[
+            {
+                path:"/",
+                element: <Home></Home>
+            },
+            {
+                path:"/coffees",
+                element: <Coffees></Coffees>
+            },
+            {
+                path:"/dashboard",
+                element: <Dashboard></Dashboard>
+            },
+        ]
     },
 ]);
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
